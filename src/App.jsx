e772520733790d6686b789ff8609e083a473aee7
shashankdhardur <165265010+shashankdhardur@users.noreply.github.com>
2024-08-31 07:17:14 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p className=" font-bold soild text-green-900">welcome to Netprime</p>
+    </div>
+  );
+};
+
+export default App;
